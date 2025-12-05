b6c2585d99ba373fb15d05b76fdde0a79ad16daf
Geoffrey <74117676+geoffrey66280@users.noreply.github.com>
2025-12-05 19:13:51 +0100

    feat: aléatoire des dès et refacto ui à gauche

--- scripts/dice.gd
+++ scripts/dice.gd
@@ -1,17 +1,23 @@
 extends RigidBody2D
 
 func _ready():
+	$FinalDiceResult.hide()
 	linear_damp = 2
 	angular_damp = 3
 	launch()
 	
 func _process(delta: float) -> void:
 	if not sleeping and is_stopped():
+		var final_random_value = randi() % 6 + 1
 		linear_velocity = Vector2.ZERO
 		angular_velocity = 0
 		sleeping = true
 		$RollingDice.stop()
-		$RollingDice.frame = 6
+		var tex = load("res://assets/sprites/de_" + str(final_random_value) + ".png")
+		$FinalDiceResult.texture = tex
+		$RollingDice.hide()
+		$FinalDiceResult.show()
+
 		
 func launch():
 	var force = Vector2(randf_range(0, 400), randf_range(0, -600))
--- scripts/level_information.gd
+++ scripts/level_information.gd
@@ -1,16 +1,17 @@
 extends Control
 
-var actual_lvl = 1
-var bosses = {
-	1: "The great barbarian",
-	2: "The ultimate goat",
-	3: "Heat the hitter"
-}
-
-var scores = {
-	1: "100",
-	2: "300",
-	3: "500"
+var level_information = {
+	"actual_lvl": 1,
+	"bosses": {
+		1: "The great barbarian",
+		2: "The ultimate goat",
+		3: "Heat the hitter"
+	},
+	"scores": {
+		1: "100",
+		2: "300",
+		3: "500"
+	}
 }
 
 
@@ -20,10 +21,9 @@ func _ready() -> void:
 	var score = $Score
 	var reward = $Reward
 	var player_score = $PlayerScore
-	print(bosses[actual_lvl])
-	boss_name.text = bosses[actual_lvl]
-	score.text = "Goal\n" + scores[actual_lvl]
-	reward.text = "reward : " + str(actual_lvl)
+	boss_name.text = level_information["bosses"][1]
+	score.text = "Goal\n" + level_information["scores"][level_information["actual_lvl"]]
+	reward.text = "reward : " + str(level_information["actual_lvl"])
 	player_score.text = "score: 0"
 
 
