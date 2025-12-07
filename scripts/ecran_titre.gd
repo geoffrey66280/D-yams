d10e692afabc9c3aa5a43b0ab3cbe3c200008806
Sid Ksouri <sid.ksouri@gmail.com>
2025-12-07 23:49:21 +0100

    ecran titre v2

--- scripts/ecran_titre.gd
+++ scripts/ecran_titre.gd
@@ -9,7 +9,7 @@ func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_options_button_pressed():
-	menu_options.visible = true
+	menu_options.visible = not menu_options.visible
 
 func _on_exit_button_pressed():
 	get_tree().quit()
