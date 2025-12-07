311de526a779fba234c8d25060b2085734abacbd
Geoffrey <74117676+geoffrey66280@users.noreply.github.com>
2025-12-07 20:01:07 +0100

    feat: get combinations

--- scripts/throwing_dices.gd
+++ scripts/throwing_dices.gd
@@ -103,4 +103,9 @@ func _on_keep_button_button_up() -> void:
 
 
 func _on_score_button_button_up() -> void:
-	pass # Replace with function body.
+	var pop_up_score = get_node("../../popup_score")
+	if(pop_up_score.visible == false):
+		pop_up_score.change_combinations_visibility(dices_kept)
+		pop_up_score.visible = true
+	else:
+		pop_up_score.visible = false
